72cce99318b1471d3d45a8cefa8e00fdd9c55d7e
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-07-12 13:21:56 +0200

    inicio del proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [linea1, setLinea1] = useState("");
+  const [linea2, setLinea2] = useState("");
+  const [imagen, setImagen] = useState("");
+
+  const onChangeLinea1 = (evento) => {
+    setLinea1(evento.target.value);
+  };
+
+  const onChangeLinea2 = (evento) => {
+    setLinea2(evento.target.value);
+  };
+  const onChangeImagen = (evento) => {
+    setImagen(evento.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={onChangeImagen}>
+        <option value="fire">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History Channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Philosoraptor</option>
+        <option value="smart">Smart Guy</option>
+      </select>
+      <br />
+      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1" />
+      <br />
+      <input onChange={onChangeLinea2} type="text" placeholder="Linea 2" />
+      <br />
+      <button>Exportar</button>
+      <div>
+        <span>{linea1}</span>
+        <br />
+        <span>{linea2}</span>
+        <img src="" alt="" />
+      </div>
     </div>
   );
 }
