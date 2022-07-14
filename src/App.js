cba239684db304918b3aad8bf6931e6b5910d57b
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-07-14 18:52:35 +0200

    exportar meme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import html2canvas from "html2canvas";
 import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [linea1, setLinea1] = useState("");
   const [linea2, setLinea2] = useState("");
-  const [imagen, setImagen] = useState("");
+  const [imagen, setImagen] = useState("fire");
 
   const onChangeLinea1 = (evento) => {
     setLinea1(evento.target.value);
@@ -15,6 +16,15 @@ function App() {
   };
   const onChangeImagen = (evento) => {
     setImagen(evento.target.value);
+  };
+  const onClickExportar = () => {
+    html2canvas(document.querySelector("#meme")).then((canvas) => {
+      let img = canvas.toDataURL("image/png");
+      let link = document.createElement("a");
+      link.download = "meme.png";
+      link.href = img;
+      link.click();
+    });
   };
 
   return (
@@ -32,12 +42,12 @@ function App() {
       <br />
       <input onChange={onChangeLinea2} type="text" placeholder="Linea 2" />
       <br />
-      <button>Exportar</button>
-      <div>
+      <button onClick={onClickExportar}>Exportar</button>
+      <div className="meme" id="meme">
         <span>{linea1}</span>
         <br />
         <span>{linea2}</span>
-        <img src="" alt="" />
+        <img src={"/img/" + imagen + ".jpg"} alt="" />
       </div>
     </div>
   );
